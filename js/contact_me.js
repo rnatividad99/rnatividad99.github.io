fbf30f73397bb3735e5b3dae1637c7a359c149aa
rnatividad99 <141118042+rnatividad99@users.noreply.github.com>
2024-10-25 09:12:41 +0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Initialize EmailJS with your Public Key
   emailjs.init("sUyADb8v1bhtxCxoo");
-  console.log(emailjs); // Check if EmailJS is loaded
+  console.log(emailjs);
 });
 
 document.getElementById('contactForm').addEventListener('submit', function(event) {
@@ -10,11 +9,19 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   // Send email using EmailJS
   emailjs.sendForm('service_oxzd5ok', 'template_32lplaf', this)
       .then(function() {
-          alert('Email sent successfully!');
+          showMessage('success', 'Your message has been sent successfully!');
           
-          // Clear the form fields after a successful email submission
           document.getElementById('contactForm').reset();
       }, function(error) {
-          alert('Failed to send email: ' + JSON.stringify(error));
+          showMessage('error', 'Failed to send the message. Please try again later.');
       });
 });
+
+function showMessage(type, message) {
+  const formMessage = document.getElementById('formMessage');
+  formMessage.innerHTML = `<div class="alert alert-${type}">${message}</div>`;
+  
+  setTimeout(() => {
+      formMessage.innerHTML = '';
+  }, 3000);
+}
